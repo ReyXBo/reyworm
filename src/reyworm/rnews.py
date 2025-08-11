174de8bd529d72bbbcad81ad336316e8045dd867
ReyXBo <reyxbo@163.com>
2025-08-11 15:10:03 +0800

    Update.

--- src/reyworm/rnews.py
+++ src/reyworm/rnews.py
@@ -12,6 +12,7 @@
 from typing import Any, Literal
 from fake_useragent import UserAgent
 from reykit.rnet import request, join_url
+from reykit.rtime import now
 
 
 __all__ = (
@@ -36,10 +37,22 @@ def get_weibo_hot_search() -> list[dict[Literal['rank', 'time', 'title', 'type',
     """
 
     # Request.
-    url = 'https://weibo.com/ajax/side/hotSearch'
+    url = 'https://weibo.com/ajax/side/searchBand'
+    timestamp_second = int(now('timestamp') / 10)
+    params = {
+        'type': 'hot',
+        'last_tab': 'hot',
+        'last_table_time': timestamp_second
+    }
     ua = UserAgent()
-    headers = {'user-agent': ua.edge}
-    response = request(url, headers=headers, check=True)
+    headers = {
+        'cookie': 'SUB=_2AkMfz4Opf8NxqwFRmvoWyGPnZIh0zA_EieKpk3JyJRMxHRl-yT9yqmEItRB6NE-teHuyVk5ZEiU3azHTQckY6H3Ale7I; '
+            'XSRF-TOKEN=fijm7qRcprz35OqHAiOSs_s1; '
+            'WBPSESS=aEftxDBVPukTPk6-ZoWBoFyWKR9WXuoQPJZmP-r3bkFMPJn3Wjg95F_I7ilFxmRpNYa5qqAbIcLktpnZwknv0Fhbp7wuHdtQY0torFGlZa26JERUGb_Bdbs5Lf-nW8nk',
+        'referer': 'https://weibo.com/newlogin?tabtype=weibo&gid=102803&openLoginLayer=0&url=https%3A%2F%2Fwww.weibo.com%2F',
+        'user-agent': ua.edge,
+    }
+    response = request(url, params, headers=headers, check=True)
 
     # Extract.
     response_json = response.json()
